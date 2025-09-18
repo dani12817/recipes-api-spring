b392b6dd67daaef8790d679d11c6b09bfb8b4caf
Daniel Leal Pérez <dani.castellut@gmail.com>
2025-09-18 20:37:46 +0200

    Initial commit

---
+++ src/main/java/com/recipes/api/spring/RecipesApiSpringApplication.java
@@ -0,0 +1,13 @@
+package com.recipes.api.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipesApiSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipesApiSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/recipes/api/spring/RecipesApiSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recipes.api.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipesApiSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
